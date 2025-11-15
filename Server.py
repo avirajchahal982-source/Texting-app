d96a8b921563a0f930077d136298874094dca727
michaelm2o9-debug <michaelm2o9@gmail.com>
2025-11-15 13:38:29 -0800

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,5 +1,7 @@
-import socketserver
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
 def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
-    server_address = ('676.7.6.7', 676767)
+    server_address = ('6.7.6.7', 8080)  # valid IP and port
     httpd = server_class(server_address, handler_class)
+    print(f"Serving on http://{server_address[0]}:{server_address[1]}")
     httpd.serve_forever()
