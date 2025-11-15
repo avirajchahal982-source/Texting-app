edca142d509cd2111a2720b682eb26535c054203
PugCapy6769 <grcleo.13@gmail.com>
2025-11-15 12:46:56 -0800

    Update server IP and port in Client.py

--- Client.py
+++ Client.py
@@ -1,9 +1,9 @@
 import socket
 import keyboard
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
-while true:
+HOST = '676.7.6.7'  # The server's hostname or IP address
+PORT = 676767        # The port used by the server
+while True:
   message = input("Type your message")
   with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
