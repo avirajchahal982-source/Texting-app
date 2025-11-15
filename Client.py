afc65ae8fd93d95661d6888f611430b856601243
michaelm2o9-debug <michaelm2o9@gmail.com>
2025-11-15 13:39:48 -0800

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,14 +1,19 @@
 import socket
 import keyboard
 
-HOST = '676.7.6.7'  # The server's hostname or IP address
-PORT = 676767        # The port used by the server
+HOST = '127.0.0.1'   # Replace with real server IP
+PORT = 8080          # Valid port number
+
 while True:
-  message = input("Type your message")
-  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'message')
-    data = s.recv(1024)
-  print('Received', repr(data))
-  if keyboard.is_pressed("esc"):
-    break
+    message = input("Type your message: ")
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(message.encode())   # send the actual user input
+        data = s.recv(1024)
+
+    print('Received:', data.decode())
+
+    if keyboard.is_pressed("esc"):
+        print("Exiting...")
+        break
